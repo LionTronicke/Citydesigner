5ce34f4002a7cdc9181775b0d44c6d086b94897a
LionTronicke <liontronicke@pm.me>
2026-05-12 20:49:27 +0200

    Add API handler for listing models

---
+++ api/list-models.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  const apiKey = process.env.GEMINI_API_KEY;
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`
+  );
+  const data = await response.json();
+  res.status(200).json(data.models?.map(m => ({
+    name: m.name,
+    methods: m.supportedGenerationMethods
+  })));
+}
